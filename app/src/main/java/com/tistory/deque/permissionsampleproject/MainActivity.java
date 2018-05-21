20453ea1563b4bbb51da0848c4207262db4621b9
Oh Seung-hoon <stdsalmon@gmail.com>
2018-05-21 19:55:45 +0900

    fix : code style

--- app/src/main/java/com/tistory/deque/permissionsampleproject/MainActivity.java
+++ app/src/main/java/com/tistory/deque/permissionsampleproject/MainActivity.java
@@ -15,41 +15,41 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-  private String[] permissions = {
-    Manifest.permission.READ_EXTERNAL_STORAGE,
-    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-    Manifest.permission.CAMERA
-  }; // 권한 요청을 받을 권한 배열
+    private String[] permissions = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    }; // 권한 요청을 받을 권한 배열
 
-  @BindView(R.id.permissionBtn)
-  Button permissionBtn;
-  @BindView(R.id.mainLayout)
-  LinearLayout mainLayout;
+    @BindView(R.id.permissionBtn)
+    Button permissionBtn;
+    @BindView(R.id.mainLayout)
+    LinearLayout mainLayout;
 
-  Permission mPermission;
-  //사용법은 Permission.java를 확인하거나 Ctrl+Q를 눌리시오
+    Permission mPermission;
+    //사용법은 Permission.java를 확인하거나 Ctrl+Q를 눌리시오
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    ButterKnife.bind(this);
+        ButterKnife.bind(this);
 
-    mPermission = new Permission(this, permissions);
-    //생성자의 인자로 권한 배열을 넣어줌
+        mPermission = new Permission(this, permissions);
+        //생성자의 인자로 권한 배열을 넣어줌
 
-  }
+    }
 
-  @Override
-  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-    mPermission.requestPermissionsResult(requestCode, permissions, grantResults);
-  }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mPermission.requestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
-  @OnClick(R.id.permissionBtn)
-  public void onClickPermssionBtn(){
-    mPermission.setSnackbar(mainLayout);
-    //인자는 스낵바가 보여질 View
-    mPermission.checkPermissions();
-  }
+    @OnClick(R.id.permissionBtn)
+    public void onClickPermssionBtn() {
+        mPermission.setSnackbar(mainLayout);
+        //인자는 스낵바가 보여질 View
+        mPermission.checkPermissions();
+    }
 }
